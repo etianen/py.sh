562265d2541184d515d301402d6bd3c080a6f035
Dave Hall <dave@etianen.com>
2016-03-30 12:34:29 +0100

    Tweaking installer

--- install.sh
+++ install.sh
@@ -8,18 +8,22 @@ shopt -s nullglob
 # https://github.com/etianen/py.sh
 
 # Download the py.sh script.
-curl --location --silent "https://raw.githubusercontent.com/etianen/py.sh/master/py.sh" > py.sh
+printf "Downloading py.sh script... "
+curl --location --silent "https://raw.githubusercontent.com/etianen/py.sh/master/py.sh" > ./py.sh
+printf "done!"
 
 # Make the script executable.
-chmod +x py.sh
+chmod +x ./py.sh
 
 # Update PYSH_HELPERS_URL to point at the latest commit.
 # This ensures that the py.sh helpers will remain compatible with the py.sh script.
+printf "Configuring py.sh script..."
 LATEST_GIT_COMMIT=$(git ls-remote https://github.com/etianen/py.sh.git master | awk '{print $1;}')
-sed -i '' 's/https\:\/\/github\.com\/etianen\/py\.sh\/archive\/master\.tar\.gz/https\:\/\/github.com\/etianen\/py.sh\/archive\/'"${LATEST_GIT_COMMIT}"'.tar.gz/g' py.sh
+sed -i '' 's/https\:\/\/github\.com\/etianen\/py\.sh\/archive\/master\.tar\.gz/https\:\/\/github.com\/etianen\/py.sh\/archive\/'"${LATEST_GIT_COMMIT}"'.tar.gz/g' ./py.sh
+printf "done!"
 
 # Run the install command.
-py.sh install
+./py.sh install
 
 # Print some useful information.
 cat << EOF
