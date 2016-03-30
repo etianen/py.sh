c8049dea2f2f22ca31a81d040f42a8d27892d8f7
Dave Hall <dave@etianen.com>
2016-03-30 12:40:45 +0100

    Tweaking installer

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ chmod +x ./py.sh
 
 # Update PYSH_HELPERS_URL to point at the latest commit.
 # This ensures that the py.sh helpers will remain compatible with the py.sh script.
-printf "Configuring py.sh script..."
+printf "Configuring py.sh script... "
 LATEST_GIT_COMMIT=$(git ls-remote https://github.com/etianen/py.sh.git master | awk '{print $1;}')
 sed -i '' 's/https\:\/\/github\.com\/etianen\/py\.sh\/archive\/master\.tar\.gz/https\:\/\/github.com\/etianen\/py.sh\/archive\/'"${LATEST_GIT_COMMIT}"'.tar.gz/g' ./py.sh
 printf "done!\n"
